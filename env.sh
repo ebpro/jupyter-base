740d6fd49d667ed7587ca585d45f07d64feda41b
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-05-03 15:21:38 +0200

    fixes build scripts : docker image name.

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 REGISTRY=brunoe
-IMAGE_NAME=jupyterjava
+IMAGE_NAME=${PWD##*/}
 
 CURRENT=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . ${DIR}/env.sh
 
 docker run --rm \
-	--name jupyterjava_${PWD##*/} \
+	--name ${PWD##*/} \
 	--volume $PWD/notebooks:/home/jovyan/work \
         --publish 8888:8888 \
         --env NB_UID=$UID \
