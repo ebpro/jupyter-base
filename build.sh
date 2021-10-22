9da0b407c4022e09970944df78948fd13945662e
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-10-22 22:28:40 +0200

    reactivates cache.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . ${DIR}/env.sh
 
-docker build --no-cache \
+docker build \
 	--file Dockerfile \
 	-t ${BASE}:$SHA \
 	`[[ "$BRANCH" != "master" ]] && echo -t ${BASE}:$BRANCH` \
